d773d375333e982bbb3c678ac3e6ed48d1d66ee0
Igor Prado <igor.uba1997@gmail.com>
2023-09-14 16:28:07 -0300

    teste

--- aula55_exercicio.py
+++ aula55_exercicio.py
@@ -39,3 +39,4 @@ Faça um programa que peça o primeiro nome do usuário. Se o nome tiver 4 letra
 menos escreva "Seu nome é curto"; se tiver entre 5 e 6 letras, escreva 
 "Seu nome é normal"; maior que 6 escreva "Seu nome é muito grande". 
 """
+#sajdbasukfbskjnsdafnfasdkl
